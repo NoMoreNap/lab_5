8b4c933703ebe47a31d2f989a33c3c7a8dda692c
NoMoreNap <vv2trash@gmail.com>
2024-02-20 11:07:03 +0300

    lab 5

--- main.cpp
+++ main.cpp
@@ -4,10 +4,21 @@
 using namespace std;
 
 int main() {
-    int a = 0;
+    unsigned int a = 0;
     setlocale(LC_ALL, "Russian");
     wcout << L"Введите а >> ";
     cin >> a;
+    if (!cin.good()) {
+        wcout << L"Ошибка ввода";
+        return 0;
+    }
+
+    double max = sqrt((-1 * (1<<31)) - 1);
+    double wrapper_a = a < 0 ? a * -1 : a;
+    if (wrapper_a >= max) {
+        wcout << L"Вне рабочего диапазона";
+        return 0;
+    }
 
     Engine F(a);
     F.calc();
